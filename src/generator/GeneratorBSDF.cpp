4debc4772359bb9b1a6ba1527a96ee8c6d31be6d
Ömercan Yazici <yazici@cg.uni-saarland.de>
2020-10-09 17:11:35 +0200

    added oren nayar

--- src/generator/GeneratorBSDF.cpp
+++ src/generator/GeneratorBSDF.cpp
@@ -2,61 +2,128 @@
 #include "Logger.h"
 
 namespace IG {
+static void bsdf_error(const std::string& msg, std::ostream& os)
+{
+	IG_LOG(L_ERROR) << msg << std::endl;
+	os << "make_black_bsdf()/* ERROR */";
+}
+
+static void bsdf_diffuse(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{
+	os << "make_diffuse_bsdf(math, surf, " << ctx.extractMaterialPropertyColor(bsdf, "reflectance") << ")";
+}
+
+static void bsdf_orennayar(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{
+	os << "make_rough_diffuse_bsdf(math, surf, "
+	   << ctx.extractMaterialPropertyNumber(bsdf, "alpha", 0.0f) << ", "
+	   << ctx.extractMaterialPropertyColor(bsdf, "reflectance")
+	   << ")";
+}
+
+static void bsdf_dielectric(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{
+	os << "make_glass_bsdf(math, surf, "
+	   << ctx.extractMaterialPropertyNumber(bsdf, "ext_ior", 1.000277f) << ", "
+	   << ctx.extractMaterialPropertyNumber(bsdf, "int_ior", 1.5046f) << ", "
+	   << ctx.extractMaterialPropertyColor(bsdf, "specular_reflectance", 1.0f) << ", "
+	   << ctx.extractMaterialPropertyColor(bsdf, "specular_transmittance", 1.0f) << ")";
+}
+
+static void bsdf_conductor(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{
+	os << "make_conductor_bsdf(math, surf, "
+	   << ctx.extractMaterialPropertyNumber(bsdf, "eta", 0.63660f) << ", "
+	   << ctx.extractMaterialPropertyNumber(bsdf, "k", 2.7834f) << ", " // TODO: Better defaults?
+	   << ctx.extractMaterialPropertyColor(bsdf, "specular_reflectance", 1.0f) << ")";
+}
+
+static void bsdf_phong(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{
+	os << "make_phong_bsdf(math, surf, "
+	   << ctx.extractMaterialPropertyColor(bsdf, "specular_reflectance", 1.0f) << ", "
+	   << ctx.extractMaterialPropertyNumber(bsdf, "exponent", 30) << ")";
+}
+
+static void bsdf_blend(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{
+	if (bsdf->anonymousChildren().size() != 2) {
+		bsdf_error("Invalid blend bsdf", os);
+	} else {
+		os << GeneratorBSDF::extract(bsdf->anonymousChildren()[0], ctx) << ", "
+		   << GeneratorBSDF::extract(bsdf->anonymousChildren()[1], ctx) << ", "
+		   << ctx.extractMaterialPropertyNumber(bsdf, "weight", 0.5f) << ")";
+	}
+}
+
+static void bsdf_mask(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{
+	if (bsdf->anonymousChildren().size() != 1) {
+		bsdf_error("Invalid mask bsdf", os);
+	} else {
+		os << "make_mix_bsdf(make_passthrough_bsdf(surf), "
+		   << GeneratorBSDF::extract(bsdf->anonymousChildren()[0], ctx) << ", "
+		   << ctx.extractMaterialPropertyNumber(bsdf, "opacity", 0.5f) << ")";
+	}
+}
+
+static void bsdf_twosided(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os)
+{ /* Ignore */
+	if (bsdf->anonymousChildren().size() != 1)
+		bsdf_error("Invalid twosided bsdf", os);
+	else
+		os << GeneratorBSDF::extract(bsdf->anonymousChildren()[0], ctx);
+}
+
+static void bsdf_null(const std::shared_ptr<TPMObject>&, const GeneratorContext&, std::ostream& os)
+{
+	os << "make_black_bsdf()/* Null */";
+}
+
+static void bsdf_unknown(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext&, std::ostream& os)
+{
+	IG_LOG(L_WARNING) << "Unknown bsdf '" << bsdf->pluginType() << "'" << std::endl;
+	os << "make_black_bsdf()/* Null */";
+}
+
+using BSDFGenerator = void (*)(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx, std::ostream& os);
+static struct {
+	const char* Name;
+	BSDFGenerator Generator;
+} _generators[] = {
+	{ "diffuse", bsdf_diffuse },
+	{ "roughdiffuse", bsdf_orennayar },
+	{ "dielectric", bsdf_dielectric },
+	{ "roughdielectric", bsdf_dielectric }, /*TODO*/
+	{ "thindielectric", bsdf_dielectric },	/*TODO*/
+	{ "conductor", bsdf_conductor },
+	{ "roughconductor", bsdf_conductor }, /*TODO*/
+	{ "phong", bsdf_phong },
+	{ "plastic", bsdf_phong },		/*TODO*/
+	{ "roughplastic", bsdf_phong }, /*TODO*/
+	{ "blendbsdf", bsdf_blend },
+	{ "mask", bsdf_mask },
+	{ "twosided", bsdf_twosided },
+	{ "null", bsdf_null },
+	{ "", nullptr }
+};
+
 std::string GeneratorBSDF::extract(const std::shared_ptr<TPMObject>& bsdf, const GeneratorContext& ctx)
 {
 	std::stringstream sstream;
 
-	const auto error = [&](const std::string& msg) {
-		IG_LOG(L_ERROR) << msg << std::endl;
-		sstream << "make_black_bsdf()/* ERROR */";
-	};
-
 	if (!bsdf) {
-		sstream << "make_black_bsdf()";
-	} else if (bsdf->pluginType() == "diffuse" || bsdf->pluginType() == "roughdiffuse" /*TODO*/) {
-		sstream << "make_diffuse_bsdf(math, surf, " << ctx.extractMaterialPropertyColor(bsdf, "reflectance") << ")";
-	} else if (bsdf->pluginType() == "dielectric" || bsdf->pluginType() == "roughdielectric" /*TODO*/ || bsdf->pluginType() == "thindielectric" /*TODO*/) {
-		sstream << "make_glass_bsdf(math, surf, "
-				<< ctx.extractMaterialPropertyNumber(bsdf, "ext_ior", 1.000277f) << ", "
-				<< ctx.extractMaterialPropertyNumber(bsdf, "int_ior", 1.5046f) << ", "
-				<< ctx.extractMaterialPropertyColor(bsdf, "specular_reflectance", 1.0f) << ", "
-				<< ctx.extractMaterialPropertyColor(bsdf, "specular_transmittance", 1.0f) << ")";
-	} else if (bsdf->pluginType() == "conductor" || bsdf->pluginType() == "roughconductor" /*TODO*/) {
-		sstream << "make_conductor_bsdf(math, surf, "
-				<< ctx.extractMaterialPropertyNumber(bsdf, "eta", 0.63660f) << ", "
-				<< ctx.extractMaterialPropertyNumber(bsdf, "k", 2.7834f) << ", " // TODO: Better defaults?
-				<< ctx.extractMaterialPropertyColor(bsdf, "specular_reflectance", 1.0f) << ")";
-	} else if (bsdf->pluginType() == "phong" || bsdf->pluginType() == "plastic" /*TODO*/ || bsdf->pluginType() == "roughplastic" /*TODO*/) {
-		sstream << "make_phong_bsdf(math, surf, "
-				<< ctx.extractMaterialPropertyColor(bsdf, "specular_reflectance", 1.0f) << ", "
-				<< ctx.extractMaterialPropertyNumber(bsdf, "exponent", 30) << ")";
-	} else if (bsdf->pluginType() == "blendbsdf") {
-		if (bsdf->anonymousChildren().size() != 2) {
-			error("Invalid blend bsdf");
-		} else {
-			sstream << extract(bsdf->anonymousChildren()[0], ctx) << ", "
-					<< extract(bsdf->anonymousChildren()[1], ctx) << ", "
-					<< ctx.extractMaterialPropertyNumber(bsdf, "weight", 0.5f) << ")";
-		}
-	} else if (bsdf->pluginType() == "mask") {
-		if (bsdf->anonymousChildren().size() != 1) {
-			error("Invalid mask bsdf");
-		} else {
-			sstream << "make_mix_bsdf(make_passthrough_bsdf(surf), "
-					<< extract(bsdf->anonymousChildren()[0], ctx) << ", "
-					<< ctx.extractMaterialPropertyNumber(bsdf, "opacity", 0.5f) << ")";
-		}
-	} else if (bsdf->pluginType() == "twosided") { /* Ignore */
-		if (bsdf->anonymousChildren().size() != 1)
-			error("Invalid twosided bsdf");
-		else
-			sstream << extract(bsdf->anonymousChildren()[0], ctx);
-	} else if (bsdf->pluginType() == "null") {
-		sstream << "make_black_bsdf()/* Null */";
+		bsdf_error("No bsdf given", sstream);
 	} else {
-		IG_LOG(L_WARNING) << "Unknown bsdf '" << bsdf->pluginType() << "'" << std::endl;
-		sstream << "make_black_bsdf()/* Unknown */";
+		for (size_t i = 0; _generators[i].Generator; ++i) {
+			if (_generators[i].Name == bsdf->pluginType()) {
+				_generators[i].Generator(bsdf, ctx, sstream);
+				return sstream.str();
+			}
+		}
+		bsdf_unknown(bsdf, ctx, sstream);
 	}
+
 	return sstream.str();
 }
 } // namespace IG
