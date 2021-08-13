d5d6628a45505ee6819292be851186e78a343bc6
PearCoding <pearcoding@gmail.com>
2021-08-13 18:15:47 +0200

    Fixed some cmake problems

--- src/frontend/viewer/IO.cpp
+++ src/frontend/viewer/IO.cpp
@@ -2,7 +2,9 @@
 #include "Image.h"
 #include "Range.h"
 
+#ifndef IG_NO_EXECUTION_H
 #include <execution>
+#endif
 
 namespace IG {
 bool saveImageRGB(const std::filesystem::path& path, const float* rgb, size_t width, size_t height, float scale)
@@ -14,7 +16,7 @@ bool saveImageRGB(const std::filesystem::path& path, const float* rgb, size_t wi
 
 	const RangeS imageRange(0, width * height);
 
-	std::for_each(std::execution::par_unseq, imageRange.begin(), imageRange.end(), [&](size_t ind) {
+	const auto pixelF = [&](size_t ind) {
 		auto r = rgb[ind * 3 + 0];
 		auto g = rgb[ind * 3 + 1];
 		auto b = rgb[ind * 3 + 2];
@@ -23,7 +25,14 @@ bool saveImageRGB(const std::filesystem::path& path, const float* rgb, size_t wi
 		img.pixels[4 * ind + 1] = g * scale;
 		img.pixels[4 * ind + 2] = b * scale;
 		img.pixels[4 * ind + 3] = 1.0f;
-	});
+	};
+
+#ifndef IG_NO_EXECUTION_H
+	std::for_each(std::execution::par_unseq, imageRange.begin(), imageRange.end(), pixelF);
+#else
+	for (size_t i : imageRange)
+		pixelF(i);
+#endif
 
 	return img.save(path);
 }
@@ -36,8 +45,7 @@ bool saveImageRGBA(const std::filesystem::path& path, const float* rgb, size_t w
 	img.pixels.reset(new float[width * height * 4]);
 
 	const RangeS imageRange(0, width * height);
-
-	std::for_each(std::execution::par_unseq, imageRange.begin(), imageRange.end(), [&](size_t ind) {
+	const auto pixelF = [&](size_t ind) {
 		auto r = rgb[ind * 4 + 0];
 		auto g = rgb[ind * 4 + 1];
 		auto b = rgb[ind * 4 + 2];
@@ -47,8 +55,15 @@ bool saveImageRGBA(const std::filesystem::path& path, const float* rgb, size_t w
 		img.pixels[4 * ind + 1] = g * scale;
 		img.pixels[4 * ind + 2] = b * scale;
 		img.pixels[4 * ind + 3] = a * scale;
-	});
+	};
+
+#ifndef IG_NO_EXECUTION_H
+	std::for_each(std::execution::par_unseq, imageRange.begin(), imageRange.end(), pixelF);
+#else
+	for (size_t i : imageRange)
+		pixelF(i);
+#endif
 
 	return img.save(path);
 }
-}
+} // namespace IG
