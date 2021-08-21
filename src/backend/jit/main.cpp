b70c9d20f6203713b4878e7924a7564ba5602790
PearCoding <pearcoding@gmail.com>
2021-08-21 15:11:41 +0200

    Changed to runtime only jit

--- src/backend/jit/main.cpp
+++ src/backend/jit/main.cpp
@@ -1,12 +1,6 @@
-#include <artic/emit.h>
-#include <artic/locator.h>
-#include <artic/log.h>
-#include <artic/print.h>
+#include <anydsl_jit.h>
 
-#include <thorin/be/codegen.h>
-#include <thorin/world.h>
-
-#include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -21,50 +15,19 @@ constexpr int OPT_LEVEL = 3;
 constexpr bool DEBUG	= false;
 #endif
 
+// TODO: Change this to a library and adapt it for the several programming stages (emitter, shader, [traversal])
 int main(int argc, char** argv)
 {
-	artic::Locator locator;
-	artic::Log log(artic::log::err, &locator);
-	log.max_errors = -1;
+	std::stringstream source;
 
-	std::vector<std::string> paths;
-	std::vector<std::string> file_data;
-	for (int i = 0; ig_api[i]; ++i) {
-		paths.emplace_back(std::string("[EMBEDDED]") + ig_api_paths[i]);
-		file_data.emplace_back(ig_api[i]);
-	}
+	for (int i = 0; ig_api[i]; ++i)
+		source << ig_api[i];
 
-	thorin::World world("__internal_interface");
-	world.set(std::make_shared<thorin::Stream>(std::cerr));
+	std::string source_str = source.str();
+	int res				   = anydsl_compile(source_str.c_str(), source_str.size(), OPT_LEVEL);
 
-	artic::ast::ModDecl program;
-	bool success = artic::compile(
-		paths, file_data,
-		false,
-		true,
-		program, world, log);
-
-	log.print_summary();
-
-	if (!success)
+	if (res < 0)
 		return EXIT_FAILURE;
-
-	world.cleanup();
-	world.opt();
-
-	thorin::DeviceBackends backends(world, OPT_LEVEL, DEBUG);
-	auto emit_to_file = [&](thorin::CodeGen& cg) {
-		auto name = std::string("TEST") + cg.file_ext();
-		std::ofstream file(name);
-		if (!file)
-			artic::log::error("cannot open '{}' for writing", name);
-		else
-			cg.emit_stream(file);
-	};
-	for (auto& cg : backends.cgs) {
-		if (cg)
-			emit_to_file(*cg);
-	}
 
 	return EXIT_SUCCESS;
 }
