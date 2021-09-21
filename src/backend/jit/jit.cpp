01bc6659066099718c2adcdddb4e7264d5d88e5a
PearCoding <pearcoding@gmail.com>
2021-09-21 13:25:01 +0200

    Made sure jit is returning a function pointer

--- src/backend/jit/jit.cpp
+++ src/backend/jit/jit.cpp
@@ -16,16 +16,21 @@ constexpr bool DEBUG	= false;
 #endif
 
 // TODO: Change this to a library and adapt it for the several programming stages (emitter, shader, [traversal])
-bool ig_compile_source(const std::string& src)
+/// Compile given source together with the ignis standard library and return pointer to the main function 'ig_main'
+void* ig_compile_source(const std::string& src)
 {
 	std::stringstream source;
 
 	for (int i = 0; ig_api[i]; ++i)
 		source << ig_api[i];
-	
+
 	source << std::endl;
 	source << src;
 
 	std::string source_str = source.str();
-	return anydsl_compile(source_str.c_str(), source_str.size(), OPT_LEVEL) >= 0;
+	int ret				   = anydsl_compile(source_str.c_str(), source_str.size(), OPT_LEVEL);
+	if (ret < 0)
+		return nullptr;
+
+	return anydsl_lookup_function(ret, "ig_main");
 }
