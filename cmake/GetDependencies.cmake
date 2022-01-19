96fb9650680a9f79827c0a322b5f9fde468e0b14
PearCoding <pearcoding@gmail.com>
2022-01-19 22:39:38 +0100

    Change dependency loading

---
+++ cmake/GetDependencies.cmake
@@ -0,0 +1,64 @@
+# Contains main dependencies used everywhere
+# Specific dependencies in optional components are defined inside the component
+CPMAddPackage(
+    NAME stb
+    GITHUB_REPOSITORY nothings/stb 
+    GIT_TAG master
+    DOWNLOAD_ONLY YES
+)
+
+CPMAddPackage(
+    NAME tinyobjloader
+    GITHUB_REPOSITORY tinyobjloader/tinyobjloader
+    GIT_TAG master
+    DOWNLOAD_ONLY YES
+)
+
+CPMAddPackage(
+    NAME rapidjson
+    GITHUB_REPOSITORY Tencent/rapidjson
+    GIT_TAG master
+    DOWNLOAD_ONLY YES
+)
+
+CPMAddPackage(
+    NAME pugixml
+    GITHUB_REPOSITORY zeux/pugixml 
+    GIT_TAG master
+    EXCLUDE_FROM_ALL
+)
+
+CPMAddPackage(
+    NAME hwinfo
+    GITHUB_REPOSITORY PearCoding/hwinfo 
+    GIT_TAG main
+    EXCLUDE_FROM_ALL
+)
+
+CPMAddPackage(
+    NAME tinyexr
+    GITHUB_REPOSITORY syoyo/tinyexr 
+    GIT_TAG master
+    DOWNLOAD_ONLY YES
+)
+
+CPMAddPackage(
+    NAME tinygltf
+    GITHUB_REPOSITORY syoyo/tinygltf 
+    GIT_TAG master
+    DOWNLOAD_ONLY YES
+)
+
+CPMAddPackage(
+    NAME stpp
+    GITHUB_REPOSITORY PearCoding/stpp 
+    GIT_TAG main
+    EXCLUDE_FROM_ALL
+)
+
+CPMAddPackage(
+    NAME libbvh
+    GITHUB_REPOSITORY madmann91/bvh
+    GIT_TAG master
+    DOWNLOAD_ONLY YES
+)
