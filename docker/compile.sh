cb54584f0986cdfcf306c33e1ac8767c2e7443aa
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-08 18:36:48 +0100

    Improved docker

--- docker/compile.sh
+++ docker/compile.sh
@@ -2,7 +2,8 @@
 
 mkdir -p /ignis/build
 cd /ignis/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install ..
 cmake --build .
 cmake --install .
-ninja clean
+cd /ignis
+rm -rf build
