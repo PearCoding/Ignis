217e9020e57396d9c0b88a59e451b637df9b9aa3
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-08 17:21:47 +0100

    Added docker support

--- docker/compile.sh
+++ docker/compile.sh
@@ -2,5 +2,7 @@
 
 mkdir -p /ignis/build
 cd /ignis/build
-cmake -G Ninja -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake ..
 cmake --build .
+cmake --install .
+ninja clean
