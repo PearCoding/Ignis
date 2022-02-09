6aed3685c311b19d8e0f9c9d1ad682c721c9aa2a
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-09 16:06:01 +0100

    More tests

--- docker/compile.sh
+++ docker/compile.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # We expect a minimum instruction based on the skylake processor
-MARCH=skylake
+MARCH=haswell
 CXX_FLAGS="-march=${MARCH} -mtune=generic"
 CLANG_FLAGS="-march=${MARCH};-mtune=generic;-ffast-math"
 
@@ -9,5 +9,6 @@ mkdir -p /ignis/build
 cd /ignis/build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="${CLANG_FLAGS}" -DIG_OPTIMIZE_FOR_NATIVE=OFF -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_WITH_DEVICE_CPU_BEST=OFF -DIG_WITH_DEVICE_CPU_AVX2=ON ..
 cmake --build .
-cat /ignis/build/src/backend/driver/driver_avx2.ll
+cat /ignis/build/src/backend/driver/driver_avx2.ll | head
+cat /ignis/build/src/backend/driver/driver_avx2.ll | tail
 cmake --install .
