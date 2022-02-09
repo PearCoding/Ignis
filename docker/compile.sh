e9d14ce79e35490a767d6ea7c7e4e591c1846718
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-09 17:38:27 +0100

    Workaround

--- docker/compile.sh
+++ docker/compile.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 # We expect a minimum instruction based on the skylake processor
-MARCH=haswell
+MARCH=skylake
 CXX_FLAGS="-march=${MARCH} -mtune=generic"
 CLANG_FLAGS="-march=${MARCH};-mtune=generic;-ffast-math"
-ARTIC_FLAGS="--host-cpu=${MARCH}"
+# TODO: Attribute is fixed... this is bad
+ARTIC_FLAGS="--host-triple;x86_64-linux-gnu;--host-cpu;znver2;--host-attr;+sse2,-tsxldtrk,+cx16,+sahf,-tbm,-avx512ifma,+sha,-gfni,-fma4,-vpclmulqdq,+prfchw,+bmi2,-cldemote,+fsgsbase,-ptwrite,-amx-tile,-uintr,+popcnt,-widekl,+aes,-avx512bitalg,-movdiri,+xsaves,-avx512er,-avxvnni,-avx512vnni,-amx-bf16,-avx512vpopcntdq,-pconfig,+clwb,-avx512f,+xsavec,+clzero,-pku,+mmx,-lwp,+rdpid,-xop,+rdseed,-waitpkg,-kl,-movdir64b,+sse4a,-avx512bw,+clflushopt,+xsave,-avx512vbmi2,+64bit,-avx512vl,-serialize,-hreset,-invpcid,-avx512cd,+avx,-vaes,-avx512bf16,+cx8,+fma,-rtm,+bmi,-enqcmd,+rdrnd,+mwaitx,+sse4.1,+sse4.2,+avx2,+fxsr,+wbnoinvd,+sse,+lzcnt,+pclmul,-prefetchwt1,+f16c,+ssse3,-sgx,-shstk,+cmov,-avx512vbmi,-amx-int8,+movbe,-avx512vp2intersect,+xsaveopt,-avx512dq,+adx,-avx512pf,+sse3"
 
 mkdir -p /ignis/build
 cd /ignis/build
