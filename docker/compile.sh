94c005e83433e5a457695b93dd74aed8e665fdf2
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-09 15:18:13 +0100

    Improved flag propagation

--- docker/compile.sh
+++ docker/compile.sh
@@ -3,10 +3,11 @@
 # We expect a minimum instruction based on the skylake processor
 MARCH=skylake
 CXX_FLAGS="-march=${MARCH} -mtune=generic"
+CLANG_FLAGS="-march=${MARCH};-mtune=generic;-ffast-math"
 
 mkdir -p /ignis/build
 cd /ignis/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="-O3;-march=${MARCH};-mtune=generic;-ffast-math" -DIG_OPTIMIZE_FOR_NATIVE=OFF -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_WITH_DEVICE_CPU_BEST=OFF -DIG_WITH_DEVICE_CPU_AVX2=ON ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="${CLANG_FLAGS}" -DIG_OPTIMIZE_FOR_NATIVE=OFF -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_WITH_DEVICE_CPU_BEST=OFF -DIG_WITH_DEVICE_CPU_AVX2=ON ..
 cmake --build .
 cmake --install .
 cd /ignis
