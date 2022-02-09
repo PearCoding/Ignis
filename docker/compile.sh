dbad3ddb66adfc41f0ec24a79db2e4e7a2b962ed
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-09 16:18:07 +0100

    Changed additional artic flags

--- docker/compile.sh
+++ docker/compile.sh
@@ -4,10 +4,11 @@
 MARCH=haswell
 CXX_FLAGS="-march=${MARCH} -mtune=generic"
 CLANG_FLAGS="-march=${MARCH};-mtune=generic;-ffast-math"
+ARTIC_FLAGS="-target-cpu=${MARCH}"
 
 mkdir -p /ignis/build
 cd /ignis/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="${CLANG_FLAGS}" -DIG_OPTIMIZE_FOR_NATIVE=OFF -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_WITH_DEVICE_CPU_BEST=OFF -DIG_WITH_DEVICE_CPU_AVX2=ON ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DIG_ARTIC_CLANG_FLAGS="${CLANG_FLAGS}" -DIG_ARTIC_FLAGS="${ARTIC_FLAGS}" -DIG_OPTIMIZE_FOR_NATIVE=OFF -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_WITH_DEVICE_CPU_BEST=OFF -DIG_WITH_DEVICE_CPU_AVX2=ON ..
 cmake --build .
 cat /ignis/build/src/backend/driver/driver_avx2.ll | head
 cat /ignis/build/src/backend/driver/driver_avx2.ll | tail
