0ead37094eb514b70471e0a17dc3193729d1c4ac
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-09 14:42:42 +0100

    Added cpu feature set detection and handling

--- docker/compile.sh
+++ docker/compile.sh
@@ -6,7 +6,7 @@ CXX_FLAGS="-march=${MARCH} -mtune=generic"
 
 mkdir -p /ignis/build
 cd /ignis/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="-O3;-march=${MARCH};-mtune=generic;-ffast-math" -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_OPTIMIZE_FOR_NATIVE=OFF ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="-O3;-march=${MARCH};-mtune=generic;-ffast-math" -DIG_OPTIMIZE_FOR_NATIVE=OFF -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_WITH_DEVICE_CPU_BEST=OFF -DIG_WITH_DEVICE_CPU_AVX2=ON ..
 cmake --build .
 cmake --install .
 cd /ignis
