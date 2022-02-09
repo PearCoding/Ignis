886f96bd0e441f2016f7e7814f3bd07c96b79c30
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-09 13:21:48 +0100

    Specified standard march for docker

--- docker/compile.sh
+++ docker/compile.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
+# We expect a minimum instruction based on the skylake processor
+MARCH=skylake
+CXX_FLAGS="-Wall -Wextra -Wpedantic -march=${MARCH} -mtune=generic"
+
 mkdir -p /ignis/build
 cd /ignis/build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="-O3;-march=${MARCH};-mtune=generic;-ffast-math" -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON ..
 cmake --build .
 cmake --install .
 cd /ignis
