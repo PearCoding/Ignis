1b442d15a1efb9925a9b1fe624cbda89786430fd
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-02-09 15:53:41 +0100

    Debug push

--- docker/compile.sh
+++ docker/compile.sh
@@ -9,6 +9,5 @@ mkdir -p /ignis/build
 cd /ignis/build
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DAnyDSL_runtime_DIR=/anydsl/runtime/build/share/anydsl/cmake -DCMAKE_INSTALL_PREFIX=/ignis-install -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" -DARTIC_CLANG_FLAGS="${CLANG_FLAGS}" -DIG_OPTIMIZE_FOR_NATIVE=OFF -DIG_WITH_DEVICE_CPU_GENERIC_ALWAYS=ON -DIG_WITH_DEVICE_CPU_BEST=OFF -DIG_WITH_DEVICE_CPU_AVX2=ON ..
 cmake --build .
+cat /ignis/build/src/backend/driver/driver_avx2.ll
 cmake --install .
-cd /ignis
-rm -rf build
