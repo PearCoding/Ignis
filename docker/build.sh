62edffa8920636def4950fb070e420349a219af1
PearCoding <pearcoding@gmail.com>
2022-02-19 18:37:32 +0100

    Added CUDA docker file

--- docker/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
-DOCKER_USER=pearcoding
-
-cd $SCRIPT_DIR
-# Build all docker image flavours
-docker build -t $DOCKER_USER/ignis:latest-avx2 -f Dockerfile-AVX2 ..
-# docker build -t $DOCKER_USER/anydsl:latest-cuda -f Dockerfile-CUDA ..
