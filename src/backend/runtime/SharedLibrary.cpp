22da072170590a3b982859c509dc7d620c793557
PearCoding <pearcoding@gmail.com>
2022-05-06 13:21:03 +0200

    Handle dll error messages

--- src/backend/runtime/SharedLibrary.cpp
+++ src/backend/runtime/SharedLibrary.cpp
@@ -30,11 +30,34 @@ public:
             throw std::runtime_error(dlerror());
     }
 #elif defined(IG_OS_WINDOWS)
+    static inline std::string GetLastErrorAsString()
+    {
+        // Get the error message ID, if any.
+        DWORD errorMessageID = ::GetLastError();
+        if (errorMessageID == 0)
+            return {}; // No error message has been recorded
+
+        LPSTR messageBuffer = nullptr;
+
+        // Ask Win32 to give us the string version of that message ID.
+        // The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                     NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+
+        // Copy the error message into a std::string.
+        std::string message(messageBuffer, size);
+
+        // Free the Win32's string's buffer.
+        LocalFree(messageBuffer);
+
+        return message;
+    }
+
     explicit SharedLibraryInternal(const std::string& path)
         : Handle(LoadLibraryA(path.c_str()))
     {
-        if (!Handle) // TODO: Better use GetLastError()
-            throw std::runtime_error("Could not load library");
+        if (!Handle)
+            throw std::runtime_error(GetLastErrorAsString());
     }
 #endif
 
