13cd73f23225c3910f41436d751a68bd9e8a944c
Ömercan Yazici <pearcoding@gmail.com>
2022-05-06 13:50:28 +0200

    Fixed unicode path loading on windows

--- src/backend/runtime/SharedLibrary.cpp
+++ src/backend/runtime/SharedLibrary.cpp
@@ -23,8 +23,8 @@ public:
 #endif
 
 #ifdef USE_DLOPEN
-    explicit SharedLibraryInternal(const std::string& path)
-        : Handle(dlopen(path.c_str(), RTLD_LAZY))
+    explicit SharedLibraryInternal(const std::filesystem::path& path)
+        : Handle(dlopen(path.u8string().c_str(), RTLD_LAZY))
     {
         if (!Handle)
             throw std::runtime_error(dlerror());
@@ -35,26 +35,22 @@ public:
         // Get the error message ID, if any.
         DWORD errorMessageID = ::GetLastError();
         if (errorMessageID == 0)
-            return {}; // No error message has been recorded
+            return {};
 
+        // Format to string
         LPSTR messageBuffer = nullptr;
+        size_t size         = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                             NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
 
-        // Ask Win32 to give us the string version of that message ID.
-        // The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
-        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                                     NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-
-        // Copy the error message into a std::string.
+        // Map to std::string
         std::string message(messageBuffer, size);
-
-        // Free the Win32's string's buffer.
         LocalFree(messageBuffer);
 
         return message;
     }
 
-    explicit SharedLibraryInternal(const std::string& path)
-        : Handle(LoadLibraryA(path.c_str()))
+    explicit SharedLibraryInternal(const std::filesystem::path& path)
+        : Handle(LoadLibraryW(path.c_str()))
     {
         if (!Handle)
             throw std::runtime_error(GetLastErrorAsString());
@@ -73,22 +69,12 @@ public:
 
 SharedLibrary::SharedLibrary(const std::filesystem::path& file)
     : mPath(file)
-{
-    const std::string u8 = file.u8string();
-
 #ifdef USE_DLOPEN
-    try {
-        mInternal.reset(new SharedLibraryInternal(u8 + ".so"));
-    } catch (...) {
-        mInternal.reset(new SharedLibraryInternal(u8));
-    }
+    , mInternal(new SharedLibraryInternal(file))
 #elif defined(IG_OS_WINDOWS)
-    try {
-        mInternal.reset(new SharedLibraryInternal(u8 + ".dll"));
-    } catch (...) {
-        mInternal.reset(new SharedLibraryInternal(u8));
-    }
+    , mInternal(new SharedLibraryInternal(file))
 #endif
+{
 }
 
 void* SharedLibrary::symbol(const std::string& name) const
