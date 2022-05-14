706ec0690bcff1694f13b17a2037d468a6a3f02b
PearCoding <pearcoding@gmail.com>
2022-05-14 11:15:33 +0200

    Fixed warnings

--- src/backend/runtime/log/ConsoleLogListener.cpp
+++ src/backend/runtime/log/ConsoleLogListener.cpp
@@ -2,23 +2,23 @@
 
 namespace IG {
 
-constexpr const char* const ANSI_BLACK         = "\u001b[0;30m";
-constexpr const char* const ANSI_RED           = "\u001b[0;31m";
-constexpr const char* const ANSI_GREEN         = "\u001b[0;32m";
-constexpr const char* const ANSI_BROWN         = "\u001b[0;33m";
-constexpr const char* const ANSI_BLUE          = "\u001b[0;34m";
-constexpr const char* const ANSI_MAGENTA       = "\u001b[0;35m";
-constexpr const char* const ANSI_CYAN          = "\u001b[0;36m";
-constexpr const char* const ANSI_GRAY          = "\u001b[0;37m";
-constexpr const char* const ANSI_DARK_GRAY     = "\u001b[1;30m";
-constexpr const char* const ANSI_LIGHT_RED     = "\u001b[1;31m";
-constexpr const char* const ANSI_LIGHT_GREEN   = "\u001b[1;32m";
-constexpr const char* const ANSI_YELLOW        = "\u001b[1;33m";
-constexpr const char* const ANSI_LIGHT_BLUE    = "\u001b[1;34m";
-constexpr const char* const ANSI_LIGHT_MAGENTA = "\u001b[1;35m";
-constexpr const char* const ANSI_LIGHT_CYAN    = "\u001b[1;36m";
-constexpr const char* const ANSI_WHITE         = "\u001b[1;37m";
-constexpr const char* const ANSI_RESET         = "\u001b[0m";
+[[maybe_unused]] constexpr const char* const ANSI_BLACK         = "\u001b[0;30m";
+[[maybe_unused]] constexpr const char* const ANSI_RED           = "\u001b[0;31m";
+[[maybe_unused]] constexpr const char* const ANSI_GREEN         = "\u001b[0;32m";
+[[maybe_unused]] constexpr const char* const ANSI_BROWN         = "\u001b[0;33m";
+[[maybe_unused]] constexpr const char* const ANSI_BLUE          = "\u001b[0;34m";
+[[maybe_unused]] constexpr const char* const ANSI_MAGENTA       = "\u001b[0;35m";
+[[maybe_unused]] constexpr const char* const ANSI_CYAN          = "\u001b[0;36m";
+[[maybe_unused]] constexpr const char* const ANSI_GRAY          = "\u001b[0;37m";
+[[maybe_unused]] constexpr const char* const ANSI_DARK_GRAY     = "\u001b[1;30m";
+[[maybe_unused]] constexpr const char* const ANSI_LIGHT_RED     = "\u001b[1;31m";
+[[maybe_unused]] constexpr const char* const ANSI_LIGHT_GREEN   = "\u001b[1;32m";
+[[maybe_unused]] constexpr const char* const ANSI_YELLOW        = "\u001b[1;33m";
+[[maybe_unused]] constexpr const char* const ANSI_LIGHT_BLUE    = "\u001b[1;34m";
+[[maybe_unused]] constexpr const char* const ANSI_LIGHT_MAGENTA = "\u001b[1;35m";
+[[maybe_unused]] constexpr const char* const ANSI_LIGHT_CYAN    = "\u001b[1;36m";
+[[maybe_unused]] constexpr const char* const ANSI_WHITE         = "\u001b[1;37m";
+[[maybe_unused]] constexpr const char* const ANSI_RESET         = "\u001b[0m";
 
 ConsoleLogListener::ConsoleLogListener(bool useAnsi)
     : LogListener()
