9eb3fdb9b0601e5fcd10b16b54403b5c5080fca6
PearCoding <pearcoding@gmail.com>
2022-06-08 21:09:36 +0200

    Drastically changed light interface Light sample intensity has pdf already applied!

--- src/backend/runtime/loader/LoaderTechnique.cpp
+++ src/backend/runtime/loader/LoaderTechnique.cpp
@@ -144,7 +144,7 @@ static void path_header_loader(std::ostream& stream, const std::string&, const s
 {
     constexpr int C = 1 /* MIS */ + 3 /* Contrib */ + 1 /* Depth */ + 1 /* Eta */;
     stream << "static RayPayloadComponents = " << C << ";" << std::endl
-           << "fn init_raypayload() = wrap_ptraypayload(PTRayPayload { mis = 0, contrib = color_builtins::white, depth = 1, eta = 1 });" << std::endl;
+           << "fn init_raypayload() = wrap_ptraypayload(init_ptraypayload());" << std::endl;
 }
 
 /////////////////////////
@@ -184,7 +184,7 @@ static void volpath_header_loader(std::ostream& stream, const std::string&, cons
 {
     constexpr int C = 1 /* MIS */ + 3 /* Contrib */ + 1 /* Depth */ + 1 /* Eta */ + 1 /* Medium */;
     stream << "static RayPayloadComponents = " << C << ";" << std::endl
-           << "fn init_raypayload() = wrap_vptraypayload(VPTRayPayload { mis = 0, contrib = color_builtins::white, depth = 1, eta = 1, medium = -1 });" << std::endl;
+           << "fn init_raypayload() = wrap_vptraypayload(init_vptraypayload());" << std::endl;
 }
 
 /////////////////////////////////
