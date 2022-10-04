d65c34ab83052bd3d6a9b56b901b9152d525eb99
Ömercan Yazici <yazici@cg.uni-saarland.de>
2022-10-04 14:29:33 +0200

    Updated benchmark script

--- scripts/benchmark_branch.sh
+++ scripts/benchmark_branch.sh
@@ -10,6 +10,8 @@ fi
 
 BENCH=${SCRIPT_DIR}/benchmark.sh
 
+exe1old=false
+exe2old=false
 scene=${SCRIPT_DIR}/../scenes/diamond_scene.json
 executable1=${BUILD_DIR}/bin/igcli
 executable2=${BUILD_DIR}/bin/igcli
@@ -43,6 +45,8 @@ while [ -n "$1" ]; do
         executable2="$2"
         shift
         ;;
+    -o1) exe1old=true ;;
+    -o2) exe2old=true ;;
     --)
         shift
         break
@@ -57,18 +61,25 @@ if [[ "${executable1}" == "${executable2}" ]]; then
     exit
 fi
 
-args=("-q --spp ${spp} -n ${num_iterations} -w ${warmup_iterations} -s ${scene} -- $@")
+echo "Benchmarking ${executable1} and ${executable2}"
 
-B1_GENERIC=$(${BENCH} -e ${executable1} ${args[@]} --target generic)
-echo "B1 [Generic] ${B1_GENERIC}"
-B1_CPU=$(${BENCH} -e ${executable1} ${args[@]} --cpu)
+args=("-q --spp ${spp} -n ${num_iterations} -w ${warmup_iterations} -s ${scene} -- $@")
+echo "Using arguments: ${args}"
+
+if [ "$exe1old" = false ]; then
+    B1_CPU=$(${BENCH} -e ${executable1} ${args[@]} --cpu)
+else
+    B1_CPU=$(${BENCH} -e ${executable1} ${args[@]} --target generic)
+fi
 echo "B1 [CPU]     ${B1_CPU}"
 B1_GPU=$(${BENCH} -e ${executable1} ${args[@]} --gpu)
 echo "B1 [GPU]     ${B1_GPU}" 
 
-B2_GENERIC=$(${BENCH} -e ${executable2} ${args[@]} --target generic)
-echo "B2 [Generic] ${B2_GENERIC}"
-B2_CPU=$(${BENCH} -e ${executable2} ${args[@]} --cpu)
+if [ "$exe2old" = false ]; then
+    B2_CPU=$(${BENCH} -e ${executable2} ${args[@]} --cpu)
+else
+    B2_CPU=$(${BENCH} -e ${executable2} ${args[@]} --target generic)
+fi
 echo "B2 [CPU]     ${B2_CPU}"
 B2_GPU=$(${BENCH} -e ${executable2} ${args[@]} --gpu)
 echo "B2 [GPU]     ${B2_GPU}" 
