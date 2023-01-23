1acb06afb068051a35bc18dc44386c0a0ed49dca
PearCoding <pearcoding@gmail.com>
2023-01-23 21:34:48 +0100

    More tests

--- src/tests/integrator/test_reproducibility.py
+++ src/tests/integrator/test_reproducibility.py
@@ -9,3 +9,12 @@ def test_basic_reproducibility():
     img1 = compute_scene(scene, spp=1, spi=1, seed=42)
     img2 = compute_scene(scene, spp=1, spi=1, seed=42)
     np.testing.assert_array_equal(img1, img2)
+
+
+def test_basic_reproducibility_spi_independent():
+    scene = create_flat_scene()
+    scene["lights"].append(
+        {"type": "point", "name": "_light", "position": [0, 0, -2], "intensity": [1, 1, 1]})
+    img1 = compute_scene(scene, spp=1, spi=1, seed=42)
+    img2 = compute_scene(scene, spp=4, spi=4, seed=42)
+    assert not np.allclose(img1, img2)
