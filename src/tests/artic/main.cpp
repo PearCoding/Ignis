8331be614c2e520c067f7127f31f6b9d86d6507f
Ömercan Yazici <pearcoding@gmail.com>
2023-02-02 11:43:39 +0100

    Make tests without gpus

--- src/tests/artic/main.cpp
+++ src/tests/artic/main.cpp
@@ -11,14 +11,15 @@
 
 #include "generated_test_interface.h"
 
-int main(int, char**)
+int main(int argc, char** argv)
 {
     // Force flush to zero mode for denormals
 #if defined(__x86_64__) || defined(__amd64__) || defined(_M_X64)
     _mm_setcsr(_mm_getcsr() | (_MM_FLUSH_ZERO_ON | _MM_DENORMALS_ZERO_ON));
 #endif
 
-    int err = test_main();
+    bool no_gpu = argc > 1 && strcmp(argv[1], "--no-gpu") == 0;
+    int err = test_main(no_gpu);
 
     if (err != 0)
         std::cout << err << " failed tests!" << std::endl;
