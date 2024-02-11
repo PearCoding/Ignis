e510cfcf2015b8382bc2e9930ee374abcacf5f1b
PearCoding <pearcoding@gmail.com>
2024-02-11 11:36:34 +0100

    fix for windows

--- scripts/setup/windows/setup_zlib.ps1
+++ scripts/setup/windows/setup_zlib.ps1
@@ -1,16 +1,22 @@
-$CURRENT=Get-Location
+$CURRENT = Get-Location
 
 cd "tmp"
 
 # Get zip
-if (!(Test-Path "zlib.zip")) {
-    Invoke-WebRequest -UserAgent "Wget" -Uri "$($Config.ZLIB_URL)" -OutFile "zlib.zip"
-    Expand-Archive zlib.zip -DestinationPath . > $null
+if (!(Test-Path "zlib*/")) {
+    if ([IO.Path]::GetExtension($Config.ZLIB_URL) -eq ".zip") {
+        Invoke-WebRequest -UserAgent "Wget" -Uri "$($Config.ZLIB_URL)" -OutFile "zlib.zip"
+        Expand-Archive zlib.zip -DestinationPath . > $null
+    }
+    else {
+        Invoke-WebRequest -UserAgent "Wget" -Uri "$($Config.ZLIB_URL)" -OutFile "zlib.tar.gz"
+        tar -xzf zlib.tar.gz -C ./
+    }
 }
-cd "zlib*/"
+Set-Location "zlib*/"
 
 # Configure
-$ZLIB_ROOT="$DEPS_ROOT\zlib".Replace("\", "/")
+$ZLIB_ROOT = "$DEPS_ROOT\zlib".Replace("\", "/")
 & $CMAKE_BIN $Config.CMAKE_EXTRA_ARGS -DCMAKE_BUILD_TYPE="$($Config.ZLIB_BUILD_TYPE)" -DCMAKE_INSTALL_PREFIX="$ZLIB_ROOT" .
 
 if ($LASTEXITCODE -ne 0) {
@@ -31,4 +37,4 @@ if ($LASTEXITCODE -ne 0) {
     throw "Failed to install zlib"
 }
 
-cd $CURRENT
+Set-Location $CURRENT
