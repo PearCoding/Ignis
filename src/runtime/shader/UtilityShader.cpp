5deeceee5abd733c364afe410b4223d6e9ec23b5
Ömercan Yazici <yazici@cg.uni-saarland.de>
2024-06-18 16:39:53 +0200

    initial modification this is not final

--- src/runtime/shader/UtilityShader.cpp
+++ src/runtime/shader/UtilityShader.cpp
@@ -10,7 +10,9 @@ std::string UtilityShader::setupTonemap(const LoaderContext& ctx)
 {
     std::stringstream stream;
 
-    stream << "#[export] fn ig_tonemap_shader(settings: &Settings, in_pixels: &[f32], out_pixels: &mut [u32], width: i32, height: i32, tonemap_settings: &TonemapSettings) -> () {" << std::endl
+    stream << "//#<include=\"driver/settings.art\">" << std::endl
+           << "//#<include=\"entrypoints/tonemap.art\">" << std::endl
+           << "#[export] fn ig_tonemap_shader(settings: &Settings, in_pixels: &[f32], out_pixels: &mut [u32], width: i32, height: i32, tonemap_settings: &TonemapSettings) -> () {" << std::endl
            << "  " << ShaderUtils::constructDevice(ctx) << std::endl
            << "  ig_tonemap_pipeline(device, in_pixels, out_pixels, width, height, tonemap_settings)" << std::endl
            << "}";
@@ -22,7 +24,8 @@ std::string UtilityShader::setupGlare(LoaderContext& ctx)
 {
     std::stringstream stream;
 
-    stream << "#[export] fn ig_glare_shader(settings: &Settings, in_pixels: &[f32], out_pixels: &mut [u32], width: i32, height: i32, glare_settings: &GlareSettings, output: &mut GlareOutput) -> () {" << std::endl
+    stream << "//#<include=\"driver/settings.art\">" << std::endl
+           << "#[export] fn ig_glare_shader(settings: &Settings, in_pixels: &[f32], out_pixels: &mut [u32], width: i32, height: i32, glare_settings: &GlareSettings, output: &mut GlareOutput) -> () {" << std::endl
            << "  " << ShaderUtils::constructDevice(ctx) << std::endl
            << ctx.Camera->generate(ctx) << std::endl // Will set `camera`
            << "  ig_glare_pipeline(device, camera, in_pixels, out_pixels, width, height, glare_settings, output)" << std::endl
@@ -35,7 +38,8 @@ std::string UtilityShader::setupImageinfo(const LoaderContext& ctx)
 {
     std::stringstream stream;
 
-    stream << "#[export] fn ig_imageinfo_shader(settings: &Settings, in_pixels: &[f32], width: i32, height: i32, ii_settings: &ImageInfoSettings, output: &mut ImageInfoOutput) -> () {" << std::endl
+    stream << "//#<include=\"driver/settings.art\">" << std::endl
+           << "#[export] fn ig_imageinfo_shader(settings: &Settings, in_pixels: &[f32], width: i32, height: i32, ii_settings: &ImageInfoSettings, output: &mut ImageInfoOutput) -> () {" << std::endl
            << "  " << ShaderUtils::constructDevice(ctx) << std::endl
            << "  ig_imageinfo_pipeline(device, in_pixels, width, height, ii_settings, output)" << std::endl
            << "}";
