e8be24c1df99922a2f2fa42146333cad581d50e1
Ömercan Yazici <yazici@cg.uni-saarland.de>
2024-06-21 13:10:35 +0200

    fix shader key

--- src/runtime/device/ShaderKey.h
+++ src/runtime/device/ShaderKey.h
@@ -17,7 +17,7 @@ public:
     inline size_t id() const
     {
         static_assert(sizeof(size_t) >= sizeof(uint32), "Expected a 64bit machine");
-        return static_cast<size_t>(mVariant) << 32 | static_cast<size_t>(mType) << 16 << static_cast<size_t>(mSubID);
+        return (static_cast<size_t>(mVariant) << 32) | (static_cast<size_t>(mType) << 16) | static_cast<size_t>(mSubID);
     }
 
     inline uint32 variant() const { return mVariant; }
