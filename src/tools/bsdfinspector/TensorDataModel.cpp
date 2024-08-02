2d4cb6ba9fad11c613d616a71870afd32b50df8d
PearCoding <pearcoding@gmail.com>
2024-08-02 23:50:33 +0200

    fix some of the mapping

--- src/tools/bsdfinspector/TensorDataModel.cpp
+++ src/tools/bsdfinspector/TensorDataModel.cpp
@@ -12,18 +12,87 @@ TensorDataModel::TensorDataModel(const std::shared_ptr<TensorTree>& ttree)
 
 TensorDataModel::~TensorDataModel() {}
 
+static inline Vector2f squareToConcentricDiskAngles(Vector2f p)
+{
+    p = 2 * p - Vector2f::Ones();
+
+    if (p.squaredNorm() <= FltEps) {
+        return Vector2f::Zero();
+    } else if (p.x() * p.x() > p.y() * p.y()) {
+        // Uses squares instead of absolute values
+        // Top half
+        float phi = Pi4 * p.y() / p.x();
+        if (!std::isfinite(phi))
+            phi = 0;
+        if (p.x() < 0)
+            phi -= Pi;
+        if (phi < 0)
+            phi += 2 * Pi;
+        return Vector2f(std::abs(p.x()), phi);
+    } else {
+        // Bottom half
+        float phi = Pi2 - Pi4 * p.x() / p.y();
+        if (!std::isfinite(phi))
+            phi = Pi2;
+        if (p.y() < 0)
+            phi -= Pi;
+        if (phi < 0)
+            phi += 2 * Pi;
+        return Vector2f(std::abs(p.y()), phi);
+    }
+}
+
+static inline Vector2f concentricDiskToSquare(const Vector2f& p)
+{
+    // const float phi = std::atan2(p.y(), p.x());
+    // float r         = p.norm();
+
+    // float a, b;
+    // if (std::abs(phi) < Pi4 || std::abs(phi) > 3 * Pi4) {
+    //     a = r = std::copysign(r, p.x());
+    //     if (p.x() < 0) {
+    //         if (p.y() < 0) {
+    //             b = (Pi + phi) * r / Pi4;
+    //         } else {
+    //             b = (phi - Pi) * r / Pi4;
+    //         }
+    //     } else {
+    //         b = (phi * r) / Pi4;
+    //     }
+    // } else {
+    //     b = r = std::copysign(r, p.y());
+    //     if (p.y() < 0) {
+    //         a = -(Pi2 + phi) * r / Pi4;
+    //     } else {
+    //         a = (Pi2 - phi) * r / Pi4;
+    //     }
+    // }
+    const bool quadrant = std::fabs(p.x()) > std::fabs(p.y());
+    const float r_sign  = quadrant ? p.x() : p.y(); // If quadrant 0 or 2
+    const float r       = std::copysign(p.squaredNorm(), r_sign);
+
+    const float phi = std::atan2(std::copysign(p.y(), p.y() * r_sign), std::copysign(p.x(), p.x() * r_sign));
+
+    const float c = 4 * phi / Pi;
+    const float t = (quadrant ? c : 2 - c) * r;
+
+    const float a = quadrant ? r : t;
+    const float b = quadrant ? t : r;
+
+    return Vector2f((a + 1) / 2, (b + 1) / 2);
+}
+
 static inline Vector2f composePoint(float incidentTheta, float incidentPhi, bool isotropic)
 {
-    incidentTheta /= Pi2;
-    if (incidentPhi < 0)
-        incidentPhi += 2 * Pi;
-    incidentPhi = std::fmod(incidentPhi, 2 * Pi) / 2 * Pi;
+    const float r = incidentTheta / Pi2;
 
     if (isotropic) {
-        const float proj = (0.5f - FltEps) - 0.5f * incidentTheta; //???
-        return Vector2f(proj, 0);
+        const float proj = (0.5f - FltEps) - 0.5f * r; //???
+        return concentricDiskToSquare(Vector2f(proj, 0));
     } else {
-        return Vector2f(incidentTheta, incidentPhi);
+        const float x = r * std::cos(incidentPhi);
+        const float y = r * std::sin(incidentPhi);
+        return concentricDiskToSquare(Vector2f(x, y));
     }
 }
 
@@ -88,13 +157,12 @@ struct QuerySubset {
         // TODO
         return QuerySubset{ .ChildOffset = 0, .NormalizedPos = pos, .Pitch = 1 };
     } else {
+        pos *= 2;
         uint32 mask = 0;
         for (int d = 0; d < 2; ++d) {
-            if (pos[d] >= 0.5f) {
+            if (pos[d] >= 1) {
                 mask |= (1 << d);
-                pos[d] = (pos[d] - 0.5f) * 2;
-            } else {
-                pos[d] = pos[d] * 2;
+                pos[d] -= 1;
             }
         }
 
@@ -109,26 +177,16 @@ static inline void drawPatch(const Vector2f& minPos, const Vector2f& maxPos, con
 {
     auto& drawList = *ImGui::GetWindowDrawList();
 
-    const float theta0 = minPos.x() * Pi2;
-    const float phi0   = minPos.y() * 2 * Pi;
-    const float theta1 = maxPos.x() * Pi2;
-    const float phi1   = maxPos.y() * 2 * Pi;
+    const Vector2f minRPhi = squareToConcentricDiskAngles(minPos);
+    const Vector2f maxRPhi = squareToConcentricDiskAngles(maxPos);
 
-    const int patches = std::max(1, (int)std::floor(64 * (phi1 - phi0) / Pi));
+    const float theta0 = minRPhi.x() * Pi2;
+    const float phi0   = minRPhi.y();
+    const float theta1 = maxRPhi.x() * Pi2;
+    const float phi1   = maxRPhi.y();
 
-    for (int i = 0; i < patches; ++i) {
-        const float t0 = i / (float)patches;
-        const float t1 = (i + 1) / (float)patches;
-        const float p0 = phi0 * (1 - t0) + phi1 * t0;
-        const float p1 = phi0 * (1 - t1) + phi1 * t1;
-
-        drawList.AddQuadFilled(transform(theta0, p0), transform(theta0, p1), transform(theta1, p1), transform(theta1, p0), ImColor(color.x(), color.y(), color.z()));
-        drawList.AddLine(transform(theta0, p0), transform(theta0, p1), LineColor);
-        drawList.AddLine(transform(theta1, p0), transform(theta1, p1), LineColor);
-    }
-
-    drawList.AddLine(transform(theta0, phi0), transform(theta1, phi0), LineColor);
-    drawList.AddLine(transform(theta0, phi1), transform(theta1, phi1), LineColor);
+    drawList.AddQuadFilled(transform(theta0, phi0), transform(theta0, phi1), transform(theta1, phi1), transform(theta1, phi0), ImColor(color.x(), color.y(), color.z()));
+    drawList.AddQuad(transform(theta0, phi0), transform(theta0, phi1), transform(theta1, phi1), transform(theta1, phi0), LineColor);
 }
 
 template <typename TransformF>
@@ -137,10 +195,11 @@ static void drawPatchRecursive(const TensorTreeNode* node, const Vector2f& incid
     const auto subset     = queryChildSubset(node, incidentPos);
     const Vector2f midPos = (minPos + maxPos) / 2;
     if (node->isLeaf()) {
-        drawPatch(minPos, midPos, mapper.apply(node->Values[0 * subset.Pitch]), transform);
-        drawPatch(Vector2f(midPos.x(), minPos.y()), Vector2f(maxPos.x(), midPos.y()), mapper.apply(node->Values[1 * subset.Pitch]), transform);
-        drawPatch(Vector2f(minPos.x(), midPos.y()), Vector2f(midPos.x(), maxPos.y()), mapper.apply(node->Values[2 * subset.Pitch]), transform);
-        drawPatch(midPos, maxPos, mapper.apply(node->Values[3 * subset.Pitch]), transform);
+        // No idea why the order is reversed in this case (see tt_lookup_leaf in tensortree.art, which is given by bsdf_t.c in Radiance)
+        drawPatch(minPos, midPos, mapper.apply(node->Values[3]), transform);
+        drawPatch(Vector2f(midPos.x(), minPos.y()), Vector2f(maxPos.x(), midPos.y()), mapper.apply(node->Values[2]), transform);
+        drawPatch(Vector2f(minPos.x(), midPos.y()), Vector2f(midPos.x(), maxPos.y()), mapper.apply(node->Values[1]), transform);
+        drawPatch(midPos, maxPos, mapper.apply(node->Values[0]), transform);
     } else {
         drawPatchRecursive(node->Children[0 * subset.Pitch].get(), subset.NormalizedPos, minPos, midPos, transform, mapper);
         drawPatchRecursive(node->Children[1 * subset.Pitch].get(), subset.NormalizedPos, Vector2f(midPos.x(), minPos.y()), Vector2f(maxPos.x(), midPos.y()), transform, mapper);
