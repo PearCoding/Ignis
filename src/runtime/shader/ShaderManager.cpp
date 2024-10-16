1f0337b4bb6f2fb11a16832de9ddea510de235d1
Ömercan Yazici <yazici@cg.uni-saarland.de>
2024-10-16 12:15:22 +0200

    Prevent progressbar with --verbose

--- src/runtime/shader/ShaderManager.cpp
+++ src/runtime/shader/ShaderManager.cpp
@@ -5,7 +5,7 @@
 #include "ShaderTaskManager.h"
 
 namespace IG {
-bool ShaderManager::compile(ScriptCompiler* compiler, size_t threads, ShaderDumpVerbosity verbosity )
+bool ShaderManager::compile(ScriptCompiler* compiler, size_t threads, ShaderDumpVerbosity verbosity)
 {
     ShaderTaskManager manager(compiler, threads, verbosity);
     ShaderReducer reducer;
@@ -34,7 +34,7 @@ bool ShaderManager::compile(ScriptCompiler* compiler, size_t threads, ShaderDump
 
     manager.finalize();
 
-    if (!IG_LOGGER.isQuiet()) {
+    if (!IG_LOGGER.isQuiet() && IG_LOGGER.verbosity() >= L_INFO /* Do not use the progressbar with debug outputs as it might clutter the console */) {
         ShaderProgressBar pb(IG_LOGGER.isUsingAnsiTerminal(), 2, numberOfUniqueEntries);
         pb.begin();
         while (!manager.isFinished()) {
